8240c67e450658655ef3ea87db7753967418ecea
akshata <akshata.malwad@gmail.com>
2018-04-05 17:30:35 +0530

    setup

--- HackathonRegister/src/main/java/com/hackathonG2/bean/HackethonUser.java
+++ HackathonRegister/src/main/java/com/hackathonG2/bean/HackethonUser.java
@@ -2,4 +2,43 @@ package com.hackathonG2.bean;
 
 public class HackethonUser {
 
+	private Long sapId;
+	private String name;
+	private String email;
+	private String skills;
+	
+	public HackethonUser(Long sapId, String name, String email, String skills) {
+		super();
+		this.sapId = sapId;
+		this.name = name;
+		this.email = email;
+		this.skills = skills;
+	}
+	
+	public Long getSapId() {
+		return sapId;
+	}
+	public void setSapId(Long sapId) {
+		this.sapId = sapId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSkills() {
+		return skills;
+	}
+	public void setSkills(String skills) {
+		this.skills = skills;
+	}
+
+	
 }
--- HackathonRegister/src/main/java/com/hackathonG2/controller/HackthonUsersController.java
+++ HackathonRegister/src/main/java/com/hackathonG2/controller/HackthonUsersController.java
@@ -1,5 +1,16 @@
 package com.hackathonG2.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
 public class HackthonUsersController {
 
+	@RequestMapping("/")
+	public ModelAndView welcome() {
+		System.out.println("IN COntroller..");
+		String message = "HELLO SPRING MVC";  
+		return new ModelAndView("welcome", "message", message);  
+	}
 }
